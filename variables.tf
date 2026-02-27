ddee9e2f252ccdcad0294ff1d191d26dad6f4159
NaveenDevOpsHub <testnaveen95508@gmail.com>
2026-02-27 09:53:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-terraform-demo"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  default = "tfstorageacctdemo"
+}
